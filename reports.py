6fe9999951ce525de962c922ee30c30d34e19ccf
Rafal Pilewicz <rafalpilewicz@gmail.com>
2020-03-20 21:43:55 +0100

    Fixed error in reports.py

--- reports.py
+++ reports.py
@@ -11,9 +11,8 @@ def generate_report(attachment, title, paragraph):
     empty_line = Spacer(1,20)
     #report_info = Paragraph(paragraph, styles["BodyText"])
     report_build = [report_title, empty_line]
-    for i, line in enumerate(paragraph.split('\n'), start = 0):
+    for i, line in enumerate(paragraph.split('\n'), start = 1):
         report_build.append(Paragraph(line, styles["BodyText"]))
-        if i > 0 and i % 2 == 0:
+        if i % 2 == 0:
             report_build.append(empty_line)
-    print(report_build)
     report.build(report_build)
