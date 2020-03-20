b12f51a368c069e47f2f0d3c0da46b5660d6a431
Rafal Pilewicz <rafalpilewicz@gmail.com>
2020-03-20 20:44:17 +0100

    Finished health_checks script and fixed emails

--- health_check.py
+++ health_check.py
@@ -4,3 +4,32 @@ import shutil
 import psutil
 import emails
 
+
+sender = "automation@example.com"
+receiver = "{}@example.com".format(os.environ.get('USER'))
+body = "Please check your system and resolve the issue as soon as possible."
+message = None
+
+#Check CPU usage
+cpu_usage = psutil.cpu_percent(interval = 60)
+if cpu_usage > 80%:
+    subject = "Error - {}".format("CPU usage is over 80%")
+    message = emails.generate_email(sender, recipient, subject, body)
+
+#Check memory usage
+mem = psutil.virtual_memory()
+THRESHOLD = 500 * 1024 * 1024  # 500MB
+if mem.available < THRESHOLD:
+    subject = "Error - {}".format("Available memory is less than 500MB")
+    message = emails.generate_email(sender, recipient, subject, body)
+
+#Check disk space availability
+dsk = shutil.disk_usage("/")
+if dsk.free < dsk.total * 20%:
+    subject = "Error - {}".format("Available disk space is less than 20%")
+    message = emails.generate_email(sender, recipient, subject, body)
+
+#Check if localhost is 127.0.0.1
+
+if message is not None:
+    emails.send_email(message)
