f4d20168ea73f10a96c225e94a446ac9c8ea7202
Rafal Pilewicz <rafalpilewicz@gmail.com>
2020-03-20 18:05:05 +0100

    Created the image transfering script

--- supplier_image_upload.py
+++ supplier_image_upload.py
@@ -4,4 +4,11 @@ import requests
 
 path = "./supplier-data/images"
 files = os.listdir(path)
+url = http://35.225.146.151/
 
+for file in files:
+    if not file.endswith(".jpeg"):
+        continue
+    image = os.path.join(path, file)
+    with open(image, 'rb') as opened:
+        r = requests.post(url, files={'file': opened})
